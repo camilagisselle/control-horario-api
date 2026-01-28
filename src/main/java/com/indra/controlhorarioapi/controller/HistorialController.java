173b12abb25e8974b08ebdb821bbc6f59494282d
Francisca <francisca.andrade2025@soymas.cl>
2026-01-28 17:17:07 -0300

    cambios historial controller

--- src/main/java/com/indra/controlhorarioapi/controller/HistorialController.java
+++ src/main/java/com/indra/controlhorarioapi/controller/HistorialController.java
@@ -3,6 +3,9 @@ package com.indra.controlhorarioapi.controller;
 import com.indra.controlhorarioapi.model.Historial;
 import com.indra.controlhorarioapi.repository.HistorialRepository;
 import com.indra.controlhorarioapi.repository.UsuarioRepository;
+
+import java.util.List;
+
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,4 +33,41 @@ public class HistorialController {
 
         return new ResponseEntity<>(historial, HttpStatus.CREATED);
     }
+
+    @GetMapping
+public ResponseEntity<List<Historial>> obtenerHistorialPorCorreo(
+        @RequestParam String correo) {
+
+    List<Historial> historial = historialRepository.findByUsuarioCorreo(correo);
+
+    if (historial.isEmpty()) {
+        return ResponseEntity.noContent().build();
+    }
+
+    return ResponseEntity.ok(historial);
+}
+    @PutMapping
+public ResponseEntity<Historial> actualizarMarcacion(
+        @RequestParam Long id,
+        @RequestBody Historial historialRequest) {
+
+    Historial historial = historialRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException(
+                    "Historial no encontrado con id = " + id));
+
+    if (historialRequest.getInicioColacion() != null) {
+        historial.setInicioColacion(historialRequest.getInicioColacion());
+    }
+
+    if (historialRequest.getFinColacion() != null) {
+        historial.setFinColacion(historialRequest.getFinColacion());
+    }
+
+    if (historialRequest.getSalida() != null) {
+        historial.setSalida(historialRequest.getSalida());
+    }
+
+    Historial actualizado = historialRepository.save(historial);
+    return ResponseEntity.ok(actualizado);
+}
 }
